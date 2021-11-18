a0197f47a49f21df6f0268421eb0eb2f379434e5
AlienDance <vlad0502vlader@gmail.com>
2021-11-19 00:29:32 +0200

    initial commit

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+
+const port = 5000;
+
+app.listen(port, () => {
+    console.log(`server started on port ${port}`);
+});
+
+app.get('/', (req, res) => {
+    res.send('My first node app!');
+});
+
+app.get('/stuff', (req, res) => {
+    res.send('Testing routing...');
+});
